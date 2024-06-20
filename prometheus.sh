ee270562460842fd7fb7bc98b40b51d32416680d
root <root@supervision>
2024-06-20 10:30:31 +0000

    premier commit
    Former-commit-id: 9ceef03b27b0b6e772721fb8403c4ece41cf4a1a

---
+++ prometheus.sh
@@ -0,0 +1,4 @@
+wget https://github.com/prometheus/prometheus/releases/download/v2.49.0-rc.1/prometheus-2.49.0-rc.1.linux-amd64.tar.gz
+tar xvfz prometheus-2.49.0-rc.1.linux-amd64.tar.gz
+cd prometheus-2.49.0-rc.1.linux-amd64.tar.gz
+./prometheus
